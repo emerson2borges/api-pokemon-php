5d0abda8e21d31369016ca9a2706b328bbf35336
Emerson Borges <emerson2borges@users.noreply.github.com>
2022-03-29 22:57:24 -0300

    feat(tipo): adiciona tipo

---
+++ app/Http/Controllers/TipoController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TipoController extends Controller
+{
+    public function createTipo(Request $request) {
+        $array = ['error' => ''];
+
+        return $array;
+    }
+    
+    public function readeAllTipos() {
+        $array = ['error' => ''];
+
+        return $array;
+    }
+    
+    public function readOneTipo(Request $request) {
+        $array = ['error' => ''];
+
+        return $array;
+    }
+
+    public function updateTipo(Request $request, $id) {
+        $array = ['error' => ''];
+
+        return $array;
+    }
+
+    public function main($id) {
+        $array = ['error' => ''];
+
+        return $array;
+    }
+}
